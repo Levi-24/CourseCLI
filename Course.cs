b909b2751ce9bda8cf1ae4dc0fa1ad1bcac9170a
Levi <leventemajor36@gmail.com>
2025-02-18 11:21:29 +0100

    Add project files.

---
+++ Course.cs
@@ -0,0 +1,28 @@
+﻿namespace CourseCLI
+{
+    internal class Course
+    {
+        public string Name { get; set; }
+        public bool Gender { get; set; }
+        public int Money { get; set; }
+        public int[] Results { get; set; }
+
+        public Course(string dataRow)
+        {
+            var pieces = dataRow.Split(';');
+            Name = pieces[0];
+            Gender = pieces[1] == "m" ? true : false;
+            Money = int.Parse(pieces[2]);
+            Results = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                Results[i] = int.Parse(pieces[i+3]);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Gender: {Gender}, Money: {Money}, Results: {Results};";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using CourseCLI;
+
+List <Course> courses = new();
+
+using StreamReader reader = new(@"../../../src/course.txt");
+while (!reader.EndOfStream)
+    courses.Add(new Course(reader.ReadLine()));
+
+Console.WriteLine("1. Feladat:");
+Console.WriteLine(courses.Count());
+
+Console.WriteLine("2. Feladat:");
+Console.WriteLine(courses.Average(x => x.Results[3]));
+
+Console.WriteLine("3. Feladat:");
+Console.WriteLine(courses.OrderByDescending(x => x.Results.Sum()).First().ToString());
+
+Console.WriteLine("4. Feladat:");
+Console.WriteLine(Convert.ToDouble(courses.Count(x => x.Gender)) / courses.Count());
+
+Console.WriteLine("5. Feladat:");
+Console.WriteLine(courses.OrderByDescending(x => x.Results[2] + x.Results[3]).Where(x => !x.Gender).First());
+
+Console.WriteLine("6. Feladat:");
+foreach (var student in courses)
+{
+    if(student.Money == 2600)
+        Console.WriteLine(student);
+}
+
+Console.WriteLine("7. Feladat:");
+
+
+Console.WriteLine("8. Feladat:");
+
+Console.WriteLine("9. Feladat:");
+
+Console.WriteLine("10. Feladat:");
